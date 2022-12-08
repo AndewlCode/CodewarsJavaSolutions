fc8b5b5c8f690489e124c08e7d88b4ede05cf9bb
AndewlCode <30481951+AndewlCode@users.noreply.github.com>
2022-12-08 21:47:30 +0300

    Codewars Java Solutions

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,22 @@ import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
-        
+
+    }
+
+    public static String bmi(double weight, double height) {
+        double bmi = weight / (height * height);
+        String result = "";
+        if (bmi <= 18.5) {
+            result = "Underweight";
+        } else if (bmi <= 25.0) {
+            result = "Normal";
+        } else if (bmi <= 30.0) {
+            result = "Overweight";
+        } else if (bmi > 30) {
+            result = "Obese";
+        }
+        return result;
     }
 
     public static int sum(int[] numbers) {
