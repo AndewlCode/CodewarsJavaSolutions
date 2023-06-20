ac329a884fb4bd58a057a4fe726d24848a3dc838
Andrey Lobachev <lobatchev.andrew@gmail.com>
2023-06-20 16:02:55 +0300

    Streams.java class updated

--- src/Classes/Streams/Streams.java
+++ src/Classes/Streams/Streams.java
@@ -2,14 +2,33 @@ package Classes.Streams;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Streams {
     public static void main(String[] args) {
-
+        System.out.println(countingSheep(5));
     }
+
+    public static String countingSheep(int num) {
+        return IntStream.rangeClosed(1, num)
+                .mapToObj(x -> Integer.parseInt(String.valueOf(x)) + " sheep...")
+                .collect(Collectors.joining());
+    }
+
+    public static int arrayPlusArray(int[] arr1, int[] arr2) {
+        return Arrays.stream(arr1).sum() + Arrays.stream(arr2).sum();
+    }
+
+
+    public static String abbrevName(String name) {
+        return Arrays.stream(name.toUpperCase().split(" "))
+                .map(s -> String.valueOf(s.charAt(0)))
+                .collect(Collectors.joining("."));
+    }
+
 
     public static int[] monkeyCount(final int n) {
         return IntStream.rangeClosed(1, n)
