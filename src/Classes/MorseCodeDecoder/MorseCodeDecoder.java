ad01d2186f3db3384ab410e97b570b3eb18ca5f0
Andrey Lobachev <andrey_lobachev@internet.ru>
2023-10-01 18:48:14 +0300

    MorseCodeDecoder.java added

---
+++ src/Classes/MorseCodeDecoder/MorseCodeDecoder.java
@@ -0,0 +1,65 @@
+package Classes.MorseCodeDecoder;
+
+import java.util.HashMap;
+
+public class MorseCodeDecoder {
+
+    public static String decode(String morseCode) {
+        HashMap<String, String> morseMap = new HashMap<>();
+        morseMap.put(".-", "A");
+        morseMap.put("-...", "B");
+        morseMap.put("-.-.", "C");
+        morseMap.put("-..", "D");
+        morseMap.put(".", "E");
+        morseMap.put("..-.", "F");
+        morseMap.put("--.", "G");
+        morseMap.put("....", "H");
+        morseMap.put("..", "I");
+        morseMap.put(".---", "J");
+        morseMap.put("-.-", "K");
+        morseMap.put(".-..", "L");
+        morseMap.put("--", "M");
+        morseMap.put("-.", "N");
+        morseMap.put("---", "O");
+        morseMap.put(".--.", "P");
+        morseMap.put("--.-", "Q");
+        morseMap.put(".-.", "R");
+        morseMap.put("...", "S");
+        morseMap.put("-", "T");
+        morseMap.put("..-", "U");
+        morseMap.put("...-", "V");
+        morseMap.put(".--", "W");
+        morseMap.put("-..-", "X");
+        morseMap.put("-.--", "Y");
+        morseMap.put("--..", "Z");
+        morseMap.put("-----", "0");
+        morseMap.put(".----", "1");
+        morseMap.put("..---", "2");
+        morseMap.put("...--", "3");
+        morseMap.put("....-", "4");
+        morseMap.put(".....", "5");
+        morseMap.put("-....", "6");
+        morseMap.put("--...", "7");
+        morseMap.put("---..", "8");
+        morseMap.put("----.", "9");
+        morseMap.put("...---...", "SOS");
+        morseMap.put("-.-.--", "!");
+        morseMap.put(".-.-.-", ".");
+
+        String toDecode = morseCode.trim();
+        String[] wordList = toDecode.trim().split(" ");
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String word : wordList) {
+            if (!word.isEmpty()) {
+                stringBuilder.append(morseMap.getOrDefault(word, word));
+            } else {
+                stringBuilder.append(word).append(" ");
+            }
+        }
+
+        String result = stringBuilder.toString();
+        result = result.trim().replaceAll(" +", " ");
+        return result;
+    }
+}
